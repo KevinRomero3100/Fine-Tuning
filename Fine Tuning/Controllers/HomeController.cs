9849b43fe0ededebf7db35b7b720142188a6fc02
KevinRomero3100 <59964519+KevinRomero3100@users.noreply.github.com>
2023-10-09 12:07:49 -0600

    Add sln

---
+++ Fine Tuning/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Fine_Tuning.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Fine_Tuning.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Fine Tuning/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Fine_Tuning.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
